c14b9b4ef7b6bc171cd7bc2f339e3134e3ef8a15
Robert Hurst <robertwhurst@gmail.com>
2015-04-06 01:29:48 -0700

    The begining of time

---
+++ cmd/rpm-generate/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import "github.com/RobertWHurst/rpm/project"
+
+func main() {
+	fmt.Println(project.Create())
+}
---
+++ cmd/rpm/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "flag"
+import "fmt"
+import "os/exec"
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) > 0 {
+		runCommand(args[0], args[1:])
+	} else {
+		fmt.Println("rpm <command> [args...]")
+	}
+}
+
+func runCommand(command string, args []string) {
+	cmd := exec.Command("rpm-"+command, args...)
+	stdout, err := cmd.Output()
+	if err != nil {
+		fmt.Println("rpm-" + command + " does not exist")
+	}
+	fmt.Print(stdout)
+}
